2402f0f893bd55da005a734731223bc5f1a2aca6
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2024-06-09 16:27:33 +0530

    ftests/010: Fix quote warnings
    Fix multiple "Q001 Single quote multiline found but double quotes
    preferred" warnings.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- tests/ftests/010-cgget-g_flag_controller_and_cgroup.py
+++ tests/ftests/010-cgget-g_flag_controller_and_cgroup.py
@@ -17,16 +17,16 @@ CONTROLLER = 'cpu'
 CGNAME = '010cgget'
 
 EXPECTED_OUT_V1 = [
-    '''cpu.cfs_period_us: 100000
+    """cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
             throttled_time 0
     cpu.shares: 1024
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # cfs_bandwidth without cpu.stat nr_busts, burst_time
-    '''cpu.cfs_burst_us: 0
+    """cpu.cfs_burst_us: 0
     cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
@@ -35,9 +35,9 @@ EXPECTED_OUT_V1 = [
     cpu.idle: 0
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # cfs_bandwidth with cpu.stat nr_busts, burst_time
-    '''cpu.cfs_burst_us: 0
+    """cpu.cfs_burst_us: 0
     cpu.cfs_period_us: 100000
     cpu.stat: nr_periods 0
             nr_throttled 0
@@ -48,11 +48,11 @@ EXPECTED_OUT_V1 = [
     cpu.idle: 0
     cpu.cfs_quota_us: -1
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max'''
+    cpu.uclamp.max: max"""
 ]
 
 EXPECTED_OUT_V2 = [
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -63,9 +63,9 @@ EXPECTED_OUT_V2 = [
     cpu.pressure: some avg10=0.00 avg60=0.00 avg300=0.00 total=0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -77,9 +77,9 @@ EXPECTED_OUT_V2 = [
             full avg10=0.00 avg60=0.00 avg300=0.00 total=0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth without cpu.stat nr_busts, burst_time
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -93,9 +93,9 @@ EXPECTED_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth with cpu.stat nr_busts, burst_time
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -111,9 +111,9 @@ EXPECTED_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max''',
+    cpu.uclamp.max: max""",
     # with PSI, cfs_bandwidth with cpu.stat nr_busts, burst_time, force_idle
-    '''cpu.weight: 100
+    """cpu.weight: 100
     cpu.stat: usage_usec 0
             user_usec 0
             system_usec 0
@@ -130,7 +130,7 @@ EXPECTED_OUT_V2 = [
     cpu.max.burst: 0
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
-    cpu.uclamp.max: max'''
+    cpu.uclamp.max: max"""
 ]
 
 
