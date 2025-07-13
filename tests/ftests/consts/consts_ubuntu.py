fb9a7b22c182b7b13580d67f7163992938e833e3
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2025-07-13 08:20:09 +0530

    ftests: Move consts.py to consts/consts_ubuntu.py
    Restructure the constants by moving consts.py to consts/consts_ubuntu.py
    as part of supporting multiple distributions with different CPU
    controller settings outputs.
    
    The original consts.py is now split into two files:
    
    - consts/consts_ubuntu.py: contains Ubuntu-specific known combinations
    of CPU controller settings.
    
    - consts/__init__.py: contains common constants shared across
    distributions, including PID controller settings.
    
    Currently, the expected CPU settings list remains the same as Ubuntu’s,
    since Ubuntu is the only supported distribution at this time.
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- tests/ftests/consts/consts_ubuntu.py
+++ tests/ftests/consts/consts_ubuntu.py
@@ -1,37 +1,12 @@
 # SPDX-License-Identifier: LGPL-2.1-only
 #
-# Constants for the libcgroup functional tests
+# Constants for the libcgroup functional tests (Ubuntu)
 #
-# Copyright (c) 2019-2021 Oracle and/or its affiliates.
+# Copyright (c) 2019-2025 Oracle and/or its affiliates.
+#
 # Author: Tom Hromatka <tom.hromatka@oracle.com>
-#
+# Author: Kamalesh Babulal <kamalesh.babulal@oracle.com>
 
-import os
-
-DEFAULT_LOG_FILE = 'libcgroup-ftests.log'
-
-LOG_CRITICAL = 1
-LOG_WARNING = 5
-LOG_DEBUG = 8
-DEFAULT_LOG_LEVEL = 5
-
-ftest_dir = os.path.dirname(os.path.abspath(__file__))
-tests_dir = os.path.dirname(ftest_dir)
-LIBCG_MOUNT_POINT = os.path.dirname(tests_dir)
-
-DEFAULT_CONTAINER_NAME = 'TestLibcg'
-DEFAULT_CONTAINER_DISTRO = 'ubuntu'
-DEFAULT_CONTAINER_RELEASE = '22.04'
-DEFAULT_CONTAINER_ARCH = 'amd64'
-DEFAULT_CONTAINER_STOP_TIMEOUT = 5
-
-TESTS_RUN_ALL = -1
-TESTS_RUN_ALL_SUITES = 'allsuites'
-TEST_PASSED = 'passed'
-TEST_FAILED = 'failed'
-TEST_SKIPPED = 'skipped'
-
-CGRULES_FILE = '/etc/cgrules.conf'
 
 EXPECTED_CPU_OUT_V1 = [
     """cpu.cfs_period_us: 100000
@@ -205,25 +180,6 @@ EXPECTED_CPU_OUT_V2 = [
     cpu.max: max 100000
     cpu.uclamp.min: 0.00
     cpu.uclamp.max: max"""
-]
-
-EXPECTED_PIDS_OUT = [
-        """pids.current: 0
-        pids.events: max 0
-        pids.max: max
-        """,
-        """pids.current: 0
-        pids.events: max 0
-        pids.max: max
-        pids.peak: 0
-        """,
-        # pids.events.local
-        """pids.current: 0
-        pids.events: max 0
-        pids.max: max
-        pids.events.local: max 0
-        pids.peak: 0
-        """
 ]
 
 # vim: set et ts=4 sw=4:
